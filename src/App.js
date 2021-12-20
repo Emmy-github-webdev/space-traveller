008ddf71dd942773117aa02deab4c14982fee3d5
Emmanuel Oga <ogahemmanuel75@gmail.com>
2021-12-20 23:29:42 +0100

    Add header

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import { Routes, Route, NavLink } from 'react-router-dom';
 import Mission from './components/Mission'
 import Rockets from './components/Rockets'
 import Profile from './components/Profile'; 
-
+import Logo from './images/planet.png';
 
 
 function App() {
   return (
     <div className="App">
     <section >
-    <nav >
-    <NavLink to="/">Rockets</NavLink>
-    <NavLink to="/Mission">Mission</NavLink>
-    <NavLink to="/Profile">My Profile</NavLink>
-     
-     
-    
+    <nav className="nav-container">
+    <div className="nav-header">
+      <img src={Logo} alt="Logo"/>
+      <h1>Space Traveller's Hub</h1>
+    </div>
+      <div className="nav-items">
+        <NavLink to="/">Rockets</NavLink>
+        <NavLink to="/Mission">Mission</NavLink>
+        <NavLink to="/Profile">My Profile</NavLink>
+      </div>
     </nav>
 
     <Routes>
