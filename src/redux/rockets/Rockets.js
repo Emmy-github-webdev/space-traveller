28bc4a424246c1ace037aa17959e71e88bf5dbb7
Emmanuel Oga <ogahemmanuel75@gmail.com>
2021-12-21 17:24:51 +0100

    Add rocket reducer

---
+++ src/redux/rockets/Rockets.js
@@ -0,0 +1,38 @@
+import {getRockets} from './RocketAPI';
+
+const GET_ROCKET = rocketStore/rockets/GET_ROCKET;
+
+const initialState = [];
+
+export const loadRocket = (payload) => ({
+  type: GET_ROCKET,
+  payload
+});
+
+export const loadRockets = () => {
+  getRockets().then((rockets) => {
+    const rocketData = [];
+    rockets.forEach((rocket) => {
+      const rocketId = {};
+      rocketId.id = rocket.rocket_id;
+      rocketId.name = rocket.rocket_name;
+      rocketId.description = rocket.description;
+      rocketId.image = rocket.flickr_images;
+      rocketId.reserved = false;
+      rocketData.push(rocketId);
+    })
+    dispatch(loadRocket(rocketData));
+  });
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ROCKET:
+      return [...state, action.payload];
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
